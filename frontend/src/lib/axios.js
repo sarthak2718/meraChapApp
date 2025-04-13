bd3edf1c9b1a45a0117fded8db316d9de636a7b3
sarthak2718 <sarthakuniyal27@gmail.com>
2025-04-13 07:53:42 +0530

    new change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api",
+  // eslint-disable-next-line no-undef
+  baseURL: process.env.NODE_ENV == 'production' ? "https://merachatapp.onrender.com/" : "http://localhost:5001/api",
   withCredentials: true,
 });
